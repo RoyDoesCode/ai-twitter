c12542c3898601ed185a5f11c6f6b68d72c15750
RoyDoesCode <roybar03@gmail.com>
2025-12-16 11:36:50 +0200

    Fix build

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import db from "@/lib/firestore";
 import twitterClient from "@/lib/twitter";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
     try {
         const id = req.nextUrl.searchParams.get("id");
