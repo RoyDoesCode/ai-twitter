141d2dafd9f791e9843db61530fb1de7d9644d3a
Roy Barzilay <roybar03@gmail.com>
2026-06-21 15:41:50 +0300

    .

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -5,7 +5,7 @@ import { runTweet } from "@/lib/inngest/functions";
 
 const serveOrigin = process.env.NEXT_PUBLIC_HOST?.replace(/\/$/, "");
 
-export const { GET, POST } = serve({
+export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [runTweet],
     serveOrigin,
